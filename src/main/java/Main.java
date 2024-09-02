e5d8dc17d7a43130f5c9b821ddb8413b213cefca
pedroferens <169068429+pedroferens@users.noreply.github.com>
2024-09-02 12:36:44 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+  public static void main(String[] args) {
+    String nome = " Pedro ferens";
+
+    String altura = 1.70;
+
+    String idade = 1;
+
+    String peso = 2009;
+
+    
+
+    System.out.printf(" minha data de nasimento é: %d/ %d/ %d/ ", dia, mes, ano);
+    System.out.printf(" o valor de pi é: %.2f" , pi);
+      
+   
+    
+  }
+
+  
+}
